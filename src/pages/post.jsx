f12f2a62ceece1da8a426db0e3d41ea348b87950
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-05-19 11:11:54 -0700

    Travis

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -1,37 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 const Posts = () => {
+    let navigate = useNavigate()
   const { id } = useParams();
   const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [searchId, setSearchId] = useState()
 
-  useEffect(() => {
-    async function fetchPosts() {
-      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+function onSearch() {
+    fetchPosts(searchId)
+}
+
+ async function fetchPosts(userId) {
+      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`)
       setPosts(data);
+      setLoading(false)
     }
 
+    function onSearchKeyPress(key) {
+        if (key === 'Enter') {
+            onSearch()
+        }
+    }
+
+
+
+  useEffect(() => {
+   
+
     fetchPosts();
-  }, [id])
+  }, [])
 
   return (
     <>
   <div className="post__search">
-    <button>← Back</button>
+    <button onClick={() => navigate('/')}>← Back</button>
     <div className="post__search--container">
       <label className="post__search--label">Search by Id</label>
       <input
-        type="number"
+        type="number" value={searchId} onChange={(event) => setSearchId(event.target.value)}
+        onKeyPress={(event) => onSearchKeyPress(event.key)}
       />
-      <button>Enter</button>
+      <button onClick={() => onSearch()}>Enter</button>
     </div>
   </div>
-  <div className="post">
-    <div className="post__title">{post.title}</div>
-    <p className="post__body">post.body</p>
-  </div>
-  <div className="post">
+  {loading ? 
+    new Array(10).fill(0).map((_, index) => (
+ <div className="post" key={index}>
     <div className="post__title">
       <div className="post__title--skeleton"></div>
     </div>
@@ -39,8 +56,18 @@ const Posts = () => {
       <p className="post__body--skeleton"></p>
     </div>
   </div>
+    
+  ))
+  : posts.map(post => (
+  <div className="post" key={post.id}>
+    <div className="post__title">{post.title}</div>
+    <p className="post__body">{post.body}</p>
+  </div>
+
+    ))}
+
 </>
-  )
+)
 }
 
 export default Posts
